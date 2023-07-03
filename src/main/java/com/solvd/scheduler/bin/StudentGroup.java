56d719cf889a1b5bade84330e9aa06c623819e5e
rinwhy <121321387+rinwhy@users.noreply.github.com>
2023-07-03 18:46:23 -0400

    Rinku (#6)
    * Updated algorithm
    
    Updated schedule to just one hashmap
    
    * Commit
    
    small changes

--- src/main/java/com/solvd/scheduler/bin/StudentGroup.java
+++ src/main/java/com/solvd/scheduler/bin/StudentGroup.java
@@ -1,12 +1,9 @@
 package com.solvd.scheduler.bin;
 
-import java.util.List;
-
 public class StudentGroup {
 
     private final int id;
     private final Schedule groupSchedule;
-    private List<Subject> syllabus;
 
     public StudentGroup(int id) {
         this.id = id;
@@ -16,14 +13,6 @@ public class StudentGroup {
 
     public int getId() {
         return id;
-    }
-
-    public List<Subject> getSyllabus() {
-        return syllabus;
-    }
-
-    public void setSyllabus(List<Subject> syllabus) {
-        this.syllabus = syllabus;
     }
 
     public Schedule getGroupSchedule() {
